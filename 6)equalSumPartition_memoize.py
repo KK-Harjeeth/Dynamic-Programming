409d92b902c794902b384053a6d4f6dc70d8caac
K K Harjeeth <107640043+KK-Harjeeth@users.noreply.github.com>
2023-09-09 22:19:58 +0530

    Update 6)equalSumPartition_memoize.py
    416. Partition Equal Subset Sum
    Medium
    11.5K
    206
    Companies
    Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
    
     
    
    Example 1:
    
    Input: nums = [1,5,11,5]
    Output: true
    Explanation: The array can be partitioned as [1, 5, 5] and [11].
    Example 2:
    
    Input: nums = [1,2,3,5]
    Output: false
    Explanation: The array cannot be partitioned into equal sum subsets.
     
    
    Constraints:
    
    1 <= nums.length <= 200
    1 <= nums[i] <= 100

--- 6)equalSumPartition_memoize.py
+++ 6)equalSumPartition_memoize.py
@@ -1,5 +1,5 @@
 def can_partition(nums):
-    # Calculate the total sum of the numbers in the input array.
+    # Calculate the total sum of the numbers in the input array..
     total_sum = sum(nums)
     
     # If the total sum is odd, it cannot be divided into two equal parts.
