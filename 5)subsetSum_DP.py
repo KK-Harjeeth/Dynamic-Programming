b4d746cc86b2f290daefa6f9e9e9e53ddfc0f951
K K Harjeeth <107640043+KK-Harjeeth@users.noreply.github.com>
2023-09-09 19:08:34 +0530

    Create 5)subsetSum_DP.py

---
+++ 5)subsetSum_DP.py
@@ -0,0 +1,19 @@
+def subset_sum_dp(nums, target):
+    n = len(nums)
+    dp = [[False] * (target + 1) for _ in range(n + 1)]
+    for i in range(n + 1):
+        dp[i][0] = True
+
+    for i in range(1, n + 1):
+        for j in range(1, target + 1):
+            if j < nums[i - 1]:
+                dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i - 1]]
+
+    return dp[n][target]
+
+# Example usage:
+nums = [3, 34, 4, 12, 5, 2]
+target = 9
+print(subset_sum_dp(nums, target))
